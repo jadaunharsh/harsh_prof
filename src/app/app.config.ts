a6552b615ada75f0641f57571ca878ab0c1af46f
jadaunharsh <jadaunharsh92@gmail.com>
2025-05-24 10:32:32 +0530

    docs folder added.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
     provideRouter(routes),
-    { provide: APP_BASE_HREF, useValue: '/' }
   ]
 };
