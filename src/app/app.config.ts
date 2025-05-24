ea6a2bc77a53779b7c45d2600406f1c31f9872e0
jadaunharsh <jadaunharsh92@gmail.com>
2025-05-24 09:43:51 +0530

    slider issue fixed, ghpages installed, app_base_ref added.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ]
 };
